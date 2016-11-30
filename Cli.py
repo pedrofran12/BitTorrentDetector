af78949c0accf2777f6d4536bf66cb5af1c775c9
Pedro Oliveira <pedrofran.oliveira@hotmail.com>
2016-11-30 18:23:46 +0000

    Cli lock

--- Cli.py
+++ Cli.py
@@ -1,10 +1,12 @@
 import os.path
 from texttable import Texttable
+import threading
 
 class Cli:
     def __init__(self):
         os.system('clear')
         self.table = Texttable()
+        self.lock = threading.Lock()
         self.table.add_row(['IP', 'MAC', 'Hostname', 'Hash', 'Torrent Description', 'Date', 'Type of Detection'])
         self.table.set_cols_align(["c", "c", "c", "c", "c", "c", "c"])
         self.table.set_cols_width([15, 17, 8, 30, 30, 19, 17])
@@ -12,9 +14,11 @@ class Cli:
         return
 
     def writeLine(self, row ):
+        self.lock.acquire()
         self.table.add_row(row)
         os.system('clear')
         print(self.table.draw())
+        self.lock.release()
         return
 
     def finish(self):
